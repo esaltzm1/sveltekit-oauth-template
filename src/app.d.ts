ffe4b1455acd2450cef67957cf20baf6581c7289
esaltzm1 <35820398+esaltzm1@users.noreply.github.com>
2024-03-25 19:33:09 -0400

    step 1 of auth

--- src/app.d.ts
+++ src/app.d.ts
@@ -3,7 +3,10 @@
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			user: import('lucia').User | null;
+			session: import('lucia').Session | null;
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
@@ -11,3 +14,4 @@ declare global {
 }
 
 export {};
+
---
+++ src/lib/server/auth.ts
@@ -0,0 +1,19 @@
+import { dev } from '$app/environment';
+import { Lucia } from 'lucia';
+
+const adapter = new BetterSQLite3Adapter(db); // your adapter
+
+export const lucia = new Lucia(adapter, {
+	sessionCookie: {
+		attributes: {
+			// set to `true` when using HTTPS
+			secure: !dev
+		}
+	}
+});
+
+declare module 'lucia' {
+	interface Register {
+		Lucia: typeof lucia;
+	}
+}
